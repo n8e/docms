59284e1a228a9e1173588c1dc89c6f32af9d5470
andela-nmartin <godmetweenciati@gmail.com>
2015-10-29 14:28:28 +0300

    modifies functions

--- documentManager.js
+++ documentManager.js
@@ -1,7 +1,30 @@
+// get the required models
+var User = require('./model/users');
+var Document = require('./model/documents');
+
 function documentManager() {
-  this.getAllUsers = function() {};
+  // to get the mongo cluster of all the users stored on the db
+  this.getAllUsers = function() {
+    User.find(function (err, users) {
+      if (err) return console.error(err);
+      console.log(users);
+    });
+  };
+
+  // to get the mongo cluster of all the user roles
   this.getAllRoles = function() {};
-  this.getAllDocuments = function() {};
+
+  // to get the mongo cluster of all the documents stored
+  this.getAllDocuments = function() {
+    Document.find(function (err, documents) {
+      if (err) return console.error(err);
+      console.log(documents);
+    });
+  };
+
+  // to get the mongo cluster of all the documents filtered by role
   this.getAllDocumentsByRole = function() {};
+
+  // to get the mongo cluster of all the documents filtered by date
   this.getAllDocumentsByDate = function() {};
 }
