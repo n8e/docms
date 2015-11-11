b72d2026422d8c7c1e19f24009e38086703200fa
andela-nmartin <godmetweenciati@gmail.com>
2015-11-11 18:44:13 +0300

    Adds missing semi colons

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ var gulp = require('gulp'),
 gulp.task('develop', function () {
   nodemon({ script: 'server.js' })
     .on('restart', function () {
-      console.log('restarted server!')
-    })
-})
+      console.log('restarted server!');
+    });
+});
 
 gulp.task('test', function () {
     return gulp.src(['./tests/documentManagerSpec.js']).pipe(jasmineNode({
